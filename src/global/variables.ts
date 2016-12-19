db013c96350ee46210cc779661f44a00aba6fcab
Atala kataych <Atala kataych>
2016-12-19 10:27:03 -0400

    Interfaces 0.1

--- src/global/variables.ts
+++ src/global/variables.ts
@@ -5,5 +5,5 @@ export var cliente: boolean = false;
 export var empresa: boolean = true;
 export var transporte: boolean = false;
 
-export var logIn: boolean = true;
-export var logOut: boolean = false;
+export var logIn: boolean = false;
+export var logOut: boolean = true;
