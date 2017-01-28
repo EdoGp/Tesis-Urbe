d7c235b74325a6908b7adde47583befcf9f71291
Eduardo Gonzalez <eduardgp@gmail.com>
2017-01-28 18:57:30 -0400

    Add files via upload
    modificaciones en los inputs y datos del ngmodel

--- src/pages/agregar-productos/agregar-productos.ts
+++ src/pages/agregar-productos/agregar-productos.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Api } from '../../providers/providers';
 
 @Component({
   selector: 'page-agregar-productos',
@@ -8,10 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class AgregarProductosPage {
 
-  constructor(public navCtrl: NavController) {}
-
+  constructor(public navCtrl: NavController, private api:Api) {}
+  producto = {
+    "Producto": "",
+        "id_usuario":  "",
+        "nombre":      "",
+        "descripcion": "",
+        "existencia":  "",
+        "costo":       ""
+  }
   ionViewDidLoad() {
     console.log('Hello AgregarProductosPage Page');
   }
 
+  postProducto(producto){
+    this.api.postProductp(producto).suscribe();
+    //console.log(this.producto);
+  }
 }
